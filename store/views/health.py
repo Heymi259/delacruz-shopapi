da1bcc826d2368a7165cde9510c983c4fca24199
unknown <heymiced34@gmail.com>
2026-05-14 20:03:03 -0500

    TESTING  CI:CD

--- store/views/health.py
+++ store/views/health.py
@@ -1,14 +1,9 @@
 # store/views/health.py
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-
-@api_view(['GET'])
-@permission_classes([AllowAny])
-def health_check(request):
-    return Response({'Service': 'ShopApi',
-                     'status': 'ok', 'version': '1.0'})
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -18,4 +13,13 @@ def health_check(request):
         'status': 'ok',
         'version': '1.0',
         'message': 'Welcome to ShopApi!'
+    })
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def testingcicd(request):
+    return Response({
+        'status': 'success',
+        'message': 'CI/CD working correctly!'
     })
