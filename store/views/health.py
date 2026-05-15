6725b9d0044fc825340cc0cf54a22243b1279409
unknown <heymiced34@gmail.com>
2026-05-14 19:37:43 -0500

    feat: implementacion CI:CD

--- store/views/health.py
+++ store/views/health.py
@@ -9,3 +9,15 @@ from rest_framework.response import Response
 def health_check(request):
     return Response({'Service': 'ShopApi',
                      'status': 'ok', 'version': '1.0'})
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def system_info(request):
+    return Response(
+        {
+            'service': 'ShopApi',
+            'status': 'running',
+            'environment': 'production',
+            'version': '1.1'
+        }
+    )
